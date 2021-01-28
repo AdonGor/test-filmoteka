0a259ddf546e54c6e79968bde86403be53a6b328
Sergij <AdonGor@users.noreply.github.com>
2021-01-28 23:13:54 +0200

    Updates

---
+++ main.2bf79b2f87307d5d6956.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{"0HMw":function(n,t,a){},QfWi:function(n,t,a){"use strict";a.r(t);a("Muwe"),a("y89P"),a("0HMw");window.onload=function(){var n=parseInt($(".cdp").attr("actpage"),10);$(".cdp_i").on("click",(function(){var t=$(this).attr("href").replace("#!","");"+1"===t?n++:"-1"===t?n--:n=parseInt(t,10),$(".cdp").attr("actpage",n)}))}}},[["QfWi",1,2]]]);
+//# sourceMappingURL=main.2bf79b2f87307d5d6956.js.map
