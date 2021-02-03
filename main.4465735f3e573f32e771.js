c68c68a01c03a6e3e108f73be951e289d474d1cf
Sergij <AdonGor@users.noreply.github.com>
2021-02-03 17:54:58 +0200

    Updates

--- main.4465735f3e573f32e771.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{"0HMw":function(n,t,a){},QfWi:function(n,t,a){"use strict";a.r(t);a("0HMw"),a("Muwe"),a("y89P");window.onload=function(){var n=parseInt($(".cdp").attr("actpage"),10);$(".cdp_i").on("click",(function(){var t=$(this).attr("href").replace("#!","");"+1"===t?n++:"-1"===t?n--:n=parseInt(t,10),$(".cdp").attr("actpage",n)}))}}},[["QfWi",1,2]]]);
-//# sourceMappingURL=main.4465735f3e573f32e771.js.map
